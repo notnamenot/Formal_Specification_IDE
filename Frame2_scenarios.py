ca5b44802e28def78f25a5dc717c4bc4f6112d99
notnamenot <izabela.pachel@wp.pl>
2021-11-19 22:51:29 +0100

    refaktor

--- Frame2_scenarios.py
+++ Frame2_scenarios.py
@@ -7,7 +7,7 @@ class FrameScenarios(LabelFrame):
 
         self.state = state
 
-        self.scenarios = []
+        self.scenarios_frames = []
 
         self.frame_input_scenario = Frame(self)
         self.frame_input_scenario.pack(side=BOTTOM)
@@ -35,106 +35,87 @@ class FrameScenarios(LabelFrame):
         # input_scenario_id.delete(0, END)
 
         self.add_scenario_frame(scenario)
-
         self.state.add_use_cases([scenario])
 
-        # TODO możliwość zmiany kolejnoości - ruszanie framem - przyciski w górę i w dół
+    # TODO możliwość zmiany kolejnoości - ruszanie framem - przyciski w górę i w dół
 
     def add_scenario_frame(self, name):
         frame_scenario = Scenario(self, name)
-        frame_scenario.bind('<Button-1>', lambda e: frame_scenario.scenario_clicked(e, name))
-
+        frame_scenario.bind('<Button-1>', lambda e: frame_scenario.scenario_clicked(e))
         frame_scenario.pack(fill=X, pady=(0, 10), padx=5)
-        self.scenarios.append(frame_scenario)
+        self.scenarios_frames.append(frame_scenario)
         return frame_scenario
 
     def refresh(self):
         print("from refresh\nall\n", self.state.all_uc_diagrams, "\ncurr diag\n", self.state.curr_uc_diagram, "\ncurr uc\n", self.state.curr_uc)
         # 1. remove current scenarios
-        for s in range(len(self.scenarios)):
-            self.scenarios[s].destroy()  # pack_forget
-
-        for i, scenario in reversed(list(enumerate(self.scenarios))):
+        for scenario in reversed(self.scenarios_frames):
             scenario.destroy()
-            self.scenarios.pop()
+            self.scenarios_frames.pop()
 
         # 2. Add new scenarios
-        for obj in self.state.curr_uc_diagram["use_cases"]:
-            frame_scenario = self.add_scenario_frame(obj['name'])
-            for step in obj["steps"]:
+        for use_case in self.state.curr_uc_diagram["use_cases"]:
+            frame_scenario = self.add_scenario_frame(use_case['name'])
+            for step in use_case["steps"]:
                 frame_scenario.add_step_frame(step["text"], step["selected_words"])
 
 
-
-
-
-# TODO checkbox na scenariuszu do którego robimy flowchart?
 class Scenario(LabelFrame):
     def __init__(self, master, name, *args, **kwargs):
         super().__init__(master, relief="flat", highlightthickness=1, *args, **kwargs)
-        self.id = ""
+        self.id = ""  # TODO uzupełniać albo wywalić
         self.name = name
 
         self.step_frames = []  # TODO trzebaby pamiętać przy usuwaniu stepa żeby stąd też czyścić
-        # self.selected_verbs = []
 
         self.frame_id_name = LabelFrame(self, relief="flat")  # relief="ridge"
         self.frame_id_name.pack(fill=X)
         self.frame_id_name.rowconfigure(1, weight=1)  # szerokość kolumny 1
         self.frame_id_name.columnconfigure(1, weight=1)  # szerokość kolumny 1
-        self.frame_id_name.bind('<Button-1>', lambda e: self.scenario_clicked(e, name))
+        self.frame_id_name.bind('<Button-1>', lambda e: self.scenario_clicked(e))
 
         self.lbl_scenario_id = Label(self.frame_id_name, text=self.id + "    ")
         self.lbl_scenario_id.grid(row=0, column=0, sticky=N + S)
-        self.lbl_scenario_id.bind('<Button-1>', lambda e: self.scenario_clicked(e, name))
+        self.lbl_scenario_id.bind('<Button-1>', lambda e: self.scenario_clicked(e))
         self.lbl_scenario_name = Label(self.frame_id_name, text=name, font=("Arial", 12))
         self.lbl_scenario_name.grid(row=0, column=1, sticky=N + S)
-        self.lbl_scenario_name.bind('<Button-1>', lambda e: self.scenario_clicked(e, name))
+        self.lbl_scenario_name.bind('<Button-1>', lambda e: self.scenario_clicked(e))
 
-        self.btn_add_step = Button(self, text="Add", command=self.click_add_step)
+        self.btn_add_step = Button(self, text="Add", command=self.add_step_clicked)
         self.btn_add_step.pack(side=BOTTOM, fill=X)
-        self.btn_add_step.bind('<Button-1>', lambda e: self.scenario_clicked(e, name))
+        self.btn_add_step.bind('<Button-1>', lambda e: self.scenario_clicked(e))
 
         self.frame_enter_step = Frame(self)
         self.frame_enter_step.pack(side=BOTTOM, fill=X)
-        self.frame_enter_step.bind('<Button-1>', lambda e: self.scenario_clicked(e, name))
+        self.frame_enter_step.bind('<Button-1>', lambda e: self.scenario_clicked(e))
 
         self.lbl_next_step_id = Label(self.frame_enter_step, text="1.")
         self.lbl_next_step_id.pack(side=LEFT)
-        self.lbl_next_step_id.bind('<Button-1>', lambda e: self.scenario_clicked(e, name))
+        self.lbl_next_step_id.bind('<Button-1>', lambda e: self.scenario_clicked(e))
 
         self.inp_step = Entry(self.frame_enter_step)
         # self.inp_step.insert(0, f'{self.number_of_steps+1}.')  # f'{len(self.step_frames)+1}.'
         # self.inp_step.insert(0, 'Enter step..')  # f'{len(self.step_frames)+1}.'
         self.inp_step.pack(side=LEFT, fill=X, expand=True)
-        self.inp_step.bind('<Button-1>', lambda e: self.scenario_clicked(e, name))
+        self.inp_step.bind('<Button-1>', lambda e: self.scenario_clicked(e))
 
-    # def scenario_clicked(self, event, uc_name):
-    def scenario_clicked(self, event, name=""):
-        # print("scenario_clicked", self.name)
+    # Every Scenario's widget has to hav explicitly assigned bind function on click
+    def scenario_clicked(self, event):
         self.master.state.set_curr_uc(self.name)
-        # print("cuc",self.master.state.curr_uc)
-        # event.widget.config(bg='red')
-        # event.widget.master.config(bg='red')
-        # self.config(bg='red')
         self.focus_force()
-        for scenario in self.master.scenarios:
+        for scenario in self.master.scenarios_frames:
             scenario.config(relief="flat")
-        self.config(relief="groove") #borderwidth=5, highlightbackground= "green",highlightthickness=1
+        self.config(relief="groove")  # borderwidth=5, highlightbackground="green", highlightthickness=1
 
-    def click_add_step(self):
-        # print("click_add_step")
+    def add_step_clicked(self):
         step_text = self.inp_step.get().strip()
 
         if not step_text:  # if empty
             return
 
         self.inp_step.delete(0, END)
-
         self.add_step_frame(step_text, [])
-
         self.master.state.add_step(step_text)
-        # print("tututu")
         # print(self.master.state.curr_uc,"\n",self.master.state.curr_uc_diagram,"\n",self.master.state.all_uc_diagrams)
 
     def add_step_frame(self, step_text, selected_words):
@@ -142,15 +123,14 @@ class Scenario(LabelFrame):
         step_frame.bind('<Button-1>', lambda e: self.scenario_clicked(e))
         step_frame.pack(fill=X)
         self.step_frames.append(step_frame)
-        self.set_lbl_next_step_id_text()
+        self.set_lbl_next_step_id()
 
-
-    def set_lbl_next_step_id_text(self):
+    def set_lbl_next_step_id(self):
         self.lbl_next_step_id.config(text=f"{len(self.step_frames)+1}.")
 
     def delete_step(self, idx):
         self.step_frames.pop(idx)
-        self.set_lbl_next_step_id_text()
+        self.set_lbl_next_step_id()
         self.renumerate_steps()
 
     def renumerate_steps(self):
@@ -170,7 +150,6 @@ class Step(Frame):
         self.lbl_id = Label(self, text=f"{self.id}.")
         self.lbl_id.bind('<Button-1>', lambda e: self.master.scenario_clicked(e))
         self.lbl_id.pack(side=LEFT)
-
 
         for i in range(len(words)+1):
             if i == len(words):  # delete button jest na końcu
@@ -203,13 +182,10 @@ class StepWordButton(Button):
     def click_function(self):
         if self['bg'] == 'white':
             self['bg'] = 'yellow'
-            self.master.master.master.state.add_selected_word(self.master.id, self['text'])
-            # self.master.master.selected_verbs.append(self['text'])  # self.master - Step, self.master.master - scenario
-
+            self.master.master.master.state.add_selected_word(self.master.id, self['text']) # self.master - Step, self.master.master - scenario, self.master.master.master - scenarios frame
         elif self['bg'] == 'yellow':
             self['bg'] = 'white'
             self.master.master.master.state.remove_selected_word(self.master.id, self['text'])
-            # self.master.master.selected_verbs.remove(self['text'])  # self.master - Step, self.master.master - scenario
 
         # print("selected_verbs word clicked: ", self.master.master.selected_verbs)
         print("selected_verbs word clicked: ", self.master.master.master.state.curr_uc)
@@ -225,7 +201,6 @@ class DeleteStepButton(Button):
         for item in self.master.winfo_children():
             if item['bg'] == 'yellow':
                 self.master.master.master.state.remove_selected_word(self.master.id, item["text"])
-                # self.master.master.selected_verbs.remove(item['text'])  # self.master.master = Scenario
         self.master.master.delete_step(self.master.id - 1)  # self.master - Step,   self.master.master - Scenario
         self.master.master.master.state.delete_step(self.master.id)
         self.master.destroy()  # self.master = Step
--- State.py
+++ State.py
@@ -3,13 +3,16 @@ XML_PATH = "xml_path"
 USE_CASES = "use_cases"
 NAME = "name"
 STEPS = "steps"
+SEQ = "seq"
+SELECTED_WORDS = "selected_words"
+TEXT = "text"
+
 
 class State:
     def __init__(self):
         self.all_uc_diagrams = []
         self.curr_uc_diagram = {}
         self.curr_uc = {}
-        # self.curr_num = -1
 
     def add_uc_diagram(self, img_path):
         self.curr_uc_diagram = {IMG_PATH: img_path, XML_PATH: "", USE_CASES: []}
@@ -44,28 +47,24 @@ class State:
     def get_curr_xml_path(self):
         return self.curr_uc_diagram[XML_PATH]
 
-    def add_use_cases(self, use_cases):
-        for use_case_name in use_cases:
-            self.curr_uc_diagram[USE_CASES].append({NAME: use_case_name, STEPS: []})
-
-    def add_step(self, step_text):
-        # self.curr_uc[STEPS].append(step_text)
-        self.curr_uc[STEPS].append({"seq": len(self.curr_uc[STEPS])+1, "text": step_text, "selected_words": []})
-
-    def delete_step(self, seq):
-        # self.curr_uc[STEPS].remove(step_text)
-        self.curr_uc[STEPS].pop(seq-1)  # -1 bo seq numerujemy od 1, a lista leci od 0
-
-        # renumerate
-        for i, step in enumerate(self.curr_uc[STEPS]):
-            print(type(step))
-            step["seq"] = i+1
-
     def set_curr_uc(self, uc_name):
         for use_case in self.curr_uc_diagram[USE_CASES]:
             if use_case[NAME] == uc_name:
                 self.curr_uc = use_case
                 break
+
+    def add_use_cases(self, use_cases):
+        for use_case_name in use_cases:
+            self.curr_uc_diagram[USE_CASES].append({NAME: use_case_name, STEPS: []})
+
+    def add_step(self, step_text):
+        self.curr_uc[STEPS].append({SEQ: len(self.curr_uc[STEPS])+1, TEXT: step_text, SELECTED_WORDS: []})
+
+    def delete_step(self, seq):
+        self.curr_uc[STEPS].pop(seq-1)  # -1 bo seq numerujemy od 1, a lista leci od 0
+        # renumerate
+        for i, step in enumerate(self.curr_uc[STEPS]):
+            step[SEQ] = i+1
 
     def contains_img(self, img_path):
         for diagram in self.all_uc_diagrams:
@@ -88,45 +87,41 @@ class State:
 
     def add_selected_word(self, step_id, word):
         for step in self.curr_uc[STEPS]:
-            if step["seq"] == step_id:
-                # print(type(step),"adding selcted word")
-                step["selected_words"].append(word)
-
+            if step[SEQ] == step_id:
+                step[SELECTED_WORDS].append(word)
 
     def remove_selected_word(self, step_id, word):
         for step in self.curr_uc[STEPS]:
-            if step["seq"] == step_id:
-                step["selected_words"].remove(word)
-#
+            if step[SEQ] == step_id:
+                step[SELECTED_WORDS].remove(word)
+
+
+
 # [
 # 	{
-# 		"img_path":  "path_to_img1",
-# 		"xml_path":  "path_to_xml1",
+# 		"img_path":  "path_to_img",
+# 		"xml_path":  "path_to_xml",
 # 		"use_cases":
 # 					[
 # 						{
-# 							"name":  "name_of_uc1_1",
-# 							"steps": [ "step1_text", "step2_text", "step3_text" ]
+# 							"name":  "Display account balance",
+# 							"steps":
+# 									[
+# 										{
+# 											"seq": 1,
+# 											"text": "System displays welcome screen",
+# 											"selected_words": ["displays"]
+# 										},
+# 										{
+# 											"seq": 2,
+# 											"text": "User inserts Card",
+# 											"selected_words": ["inserts"]
+# 										},
+# 										...
+# 									]
 # 						},
-# 						{
-# 							"name":  "name_of_uc2_1",
-# 							"steps": [ "step1_text", "step2_text", "step3_text", "step4_text" ]
-# 						}
+# 						...
 # 					]
 # 	},
-# 	{
-# 		"img_path":  "path_to_img2",
-# 		"xml_path":  "path_to_xml2",
-# 		"use_cases":
-# 					[
-# 						{
-# 							"name":  "name_of_uc1_2",
-# 							"steps": [ "step1_text", "step2_text", "step3_text" ]
-# 						},
-# 						{
-# 							"name":  "name_of_uc2_2",
-# 							"steps": [ "step1_text", "step2_text" ]
-# 						}
-# 					]
-# 	}
+# 	...
 # ]
